3bfe1347073d63fa954eafa00f4a5cc12c3cb2d9
Jean-Philippe Racine <53374126+Aklar45dev@users.noreply.github.com>
2022-03-11 14:50:40 -0500

    urls

--- src/videoUrls.js
+++ src/videoUrls.js
@@ -1,7 +1,18 @@
 const interviewVideos = [
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/Ch2+choix+1+proposer+cadenasser_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/Ch3+choix+1.1+insister_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/Ch1+choix+pas+demander_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Intro+%2B+premi%C3%A8re+question00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+2_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+3_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+4_00000022_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+5_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+6_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+7_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+8_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+9_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+10_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+11_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+12_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+13_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/conclusion00000000_1.mp4"
 ]
 
 export default {interviewVideos};
