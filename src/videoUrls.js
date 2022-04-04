ecfcf99220aceb4c95af7eded65068bbc9709608
Jean-Philippe Racine <53374126+Aklar45dev@users.noreply.github.com>
2022-04-04 10:56:27 -0400

    urls

--- src/videoUrls.js
+++ src/videoUrls.js
@@ -1,18 +1,19 @@
 const interviewVideos = [
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Intro+%2B+premi%C3%A8re+question00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+1_00000000_1.mp4",
     "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+2_00000000_1.mp4",
     "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+3_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+4_00000022_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+5_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+6_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+7_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+8_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+9_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+10_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+11_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+12_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/Question+13_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/conclusion00000000_1.mp4"
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+4_00000022_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+5_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+6_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+7_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+8_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+9_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+10_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+11_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+12_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+13_00000000_1.mp4",
+    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+14_00000000_1.mp4"
+    
 ]
 
 export default {interviewVideos};
