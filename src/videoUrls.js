386d87ec90ff64de98706961321abc63be8017cc
Jean-Philippe Racine <53374126+Aklar45dev@users.noreply.github.com>
2022-05-18 10:47:22 -0400

    vids

--- src/videoUrls.js
+++ src/videoUrls.js
@@ -5,7 +5,6 @@ const interviewVideos = [
     "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+4_00000022_1.mp4",
     "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+5_00000000_1.mp4",
     "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+6_00000000_1.mp4",
-    "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+7_00000000_1.mp4",
     "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+8_00000000_1.mp4",
     "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+9_00000000_1.mp4",
     "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/entretien/question+10_00000000_1.mp4",
